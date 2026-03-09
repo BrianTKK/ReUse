0586a010c0f11eb6af5e4420f3347a30262f7a6f
Brian Henrique <brianhenriquedp@gmail.com>
2026-03-09 13:47:51 -0400

    feat: add theme color constants

---
+++ constants/theme.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#26C9B9',        // Color1
+    greenLight: '#19E65E',     // Color2 Light
+    greenMid: '#278753',       // Color2 Mid
+    greenDark: '#1A4632',      // Color 2 Dark
+    textDark: '#1B281F',       // Text Dark
+    white: '#FFFFFF',          // Branco global
+};
