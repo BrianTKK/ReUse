0da78f07df1d34fef862c3c0649c2dc61ff69ec5
Brian Henrique <brianhenriquedp@gmail.com>
2026-03-09 20:07:55 -0400

    feat: Add Plus Jakarta Sans font and an initial typography component.

---
+++ components/Typography/typography.tsx
@@ -0,0 +1,72 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    h1: {
+        fontSize: 30,
+        lineHeight: 37.5,
+        fontWeight: '700',
+        fontFamily: 'PlusJakartaSans_700Bold',
+    },
+    h2: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: '600',
+        fontFamily: 'PlusJakartaSans_600SemiBold',
+    },
+    h3: {
+        fontSize: 20,
+        lineHeight: 28,
+        fontWeight: '600',
+        fontFamily: 'PlusJakartaSans_600SemiBold',
+    },
+    h4: {
+        fontSize: 18,
+        lineHeight: 26,
+        fontWeight: '500',
+        fontFamily: 'PlusJakartaSans_500Medium',
+    },
+    h5: {
+        fontSize: 16,
+        lineHeight: 24,
+        fontWeight: '500',
+        fontFamily: 'PlusJakartaSans_500Medium',
+    },
+    h6: {
+        fontSize: 14,
+        lineHeight: 20,
+        fontWeight: '500',
+        fontFamily: 'PlusJakartaSans_500Medium',
+        textTransform: 'uppercase',
+    },
+    body: {
+        fontSize: 18,
+        lineHeight: 29.3,
+        fontWeight: '400',
+        fontFamily: 'PlusJakartaSans_400Regular',
+    },
+    body2: {
+        fontSize: 16,
+        lineHeight: 28,
+        fontWeight: '400',
+        fontFamily: 'PlusJakartaSans_400Regular',
+        //Para descrições
+    },
+    body3: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight: '400',
+        fontFamily: 'PlusJakartaSans_400Regular',
+    },
+    button: {
+        fontSize: 16,
+        lineHeight: 24,
+        fontWeight: '600',
+        fontFamily: 'PlusJakartaSans_600SemiBold',
+    },
+    caption: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight: '400',
+        fontFamily: 'PlusJakartaSans_400Regular',
+    },
+})
